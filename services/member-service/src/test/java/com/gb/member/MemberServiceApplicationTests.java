900de4eef5debaca07bda7f438a05b19a1b39e27
Yewon0106 <yooyy0106@gmail.com>
2026-05-26 11:03:03 +0900

    Refactor: user-service를 member-service로 변경

--- services/member-service/src/test/java/com/gb/member/MemberServiceApplicationTests.java
+++ services/member-service/src/test/java/com/gb/member/MemberServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.gb.user;
+package com.gb.member;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class MemberServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
