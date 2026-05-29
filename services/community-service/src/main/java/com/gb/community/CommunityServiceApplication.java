50b2057dd92a5c5d2e725047376a43b4efadec99
Choi Seungmin <blanc_91@icloud.com>
2026-05-29 17:13:47 +0900

    Chore: community-service scanBasePackages = "com.gb" 추가
    다른 3개 서비스와 동일하게 common 모듈의 GlobalExceptionHandler가 자동
    등록되도록 스캔 범위를 넓힌다.

--- services/community-service/src/main/java/com/gb/community/CommunityServiceApplication.java
+++ services/community-service/src/main/java/com/gb/community/CommunityServiceApplication.java
@@ -3,7 +3,7 @@ package com.gb.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.gb")
 public class CommunityServiceApplication {
 
 	public static void main(String[] args) {
